2c91e2f27793fd039bcdd90c0fa8e6abdcbca07d
klbennett <https://github.com/settings/applications>
2018-10-15 13:14:32 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //Your code here
+
+//The function simply invokes the function passed through to it. It also returns the return value of the passed through function.
+function justInvoke(fn) {
+return fn(); {
+    return this.thisValue;
+}
+}
+
+// The function again invokes the function passed to it, but uses the call method to return the function's this value. (Make sure to correctly pass the third argument!)
+function setThisWithCall(fn, thisValue, arg) {
+    // console.log(arg)
+        return fn.call(thisValue, arg)
+}
+
+// Again, invoke the function passed to it, change the this value of that function passed to it. In addition, we ask you to invoked the passed function with arguments. You should accomplish all of the above by using apply.
+function setThisWithApply(fn, thisValue, arg) {
+        return fn.apply(thisValue, arg)
+}
+
+// Write a function that returns a copy of the function passed through, but sets the this value of the function's copy.
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+    return functionToBeCopied.bind(thisValue)
+}
